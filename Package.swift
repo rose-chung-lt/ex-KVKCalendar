1a3ec9bb30f9193e05565196ce9448c8ba649e36
rose.chung <rose.chung@lativ.com.tw>
2022-02-21 11:43:16 +0800

    fork ex-KVKCalendar

--- Package.swift
+++ Package.swift
@@ -4,14 +4,14 @@
 import PackageDescription
 
 let package = Package(
-    name: "KVKCalendar",
+    name: "ex-KVKCalendar",
     defaultLocalization: "en",
     platforms: [.iOS(.v10)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "KVKCalendar",
-            targets: ["KVKCalendar"]),
+            name: "ex-KVKCalendar",
+            targets: ["ex-KVKCalendar"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -21,13 +21,13 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "KVKCalendar",
+            name: "ex-KVKCalendar",
             dependencies: [],
             path: "Sources",
             exclude: ["../Example"]),
         .testTarget(
             name: "KVKCalendarTests",
-            dependencies: ["KVKCalendar"]),
+            dependencies: ["ex-KVKCalendar"]),
     ],
     swiftLanguageVersions: [.version("5")]
 )
